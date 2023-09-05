e7da4c68507bc8a05c0e7fe03408eb375a2025a7
JMH <jmh2314@gmail.com>
2023-09-05 22:14:01 +0900

    DB 변경: H2 -> MariaDB

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -42,7 +42,6 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @GetMapping("/members")
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
